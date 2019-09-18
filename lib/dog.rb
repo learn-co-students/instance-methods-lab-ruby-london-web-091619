2f6b0c7e0a79fb569f9e325a5d1921d909cc0fa0
github username <github email address>
2019-09-18 09:48:54 +0100

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def bark
+        puts "woof"
+    end
+    def sit
+        puts "sit"
+    end
+end
+
+class Person
+    def talk
+        puts "Hello World"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
